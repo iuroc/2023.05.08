175738c306ee8aef9bd788023801cefb3c4ad2eb
oyps <ponconsoft@gmail.com>
2023-02-21 19:30:49 +0800

    .

--- header.php
+++ header.php
@@ -29,6 +29,10 @@
                         </li>
                     <?php endforeach ?>
                 </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
             </div>
         </div>
     </nav>
